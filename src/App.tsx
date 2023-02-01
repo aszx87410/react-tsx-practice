84a8d62ee13ccefed5b4b41f7e2d9e6193923314
huli <aszx87410@gmail.com>
2023-02-01 21:45:43 +0800

    feat: add StepIndicator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import StepIndicator from 'components/StepIndicator'
+
 function App() {
-  return <div className="relative overflow-hidden bg-white">Initial</div>
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <div className="m-auto mt-[100px] w-[400px] rounded border-[1px] border-black">
+        <StepIndicator currentStep={1} />
+        <div className="h-[500px] pt-[100px]"></div>
+        <div className="flex">
+          <button>Previous</button>
+          <button>Next</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
